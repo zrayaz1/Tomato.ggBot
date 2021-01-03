616b6efbacd5551e5e4b0f0b28171f4af77a5b2d
ZachBrown <zrayaz12@gmail.com>
2021-01-02 23:24:47 -0700

    added logging

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import requests
 from discord.ext import commands
 from discord import *
+import logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = commands.Bot(command_prefix='$')
 colorRatingNew = {
