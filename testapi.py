1215fd9d21cd326ebb000be1063d9f992c315996
zrayaz1 <zrayaz12@gmail.com>
2021-03-16 20:24:22 -0700

    remove all the secrets

--- testapi.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-apiUrl = "https://tomatobackend.herokuapp.com/api/abcd/moetracker/get/com"
-r = requests.get(apiUrl)
-with open('moedata1.txt','wb') as text_file:
-	text_file.truncate()
-	text_file.write(r.content)
-
-
